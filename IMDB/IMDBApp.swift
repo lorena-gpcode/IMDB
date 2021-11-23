7481d6f27deb3ea47aadc07fc7603eb36a5fdd82
lorena <lorenaggama@gmail.com>
2021-11-23 15:35:40 -0300

    Initial Commit

---
+++ IMDB/IMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDBApp.swift
+//  IMDB
+//
+//  Created by Lorena Gama on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct IMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
